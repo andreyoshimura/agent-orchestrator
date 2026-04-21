c96d86b53db95eb2c570c5a16adc2df0fd73684d
Andre Yoshimura <48725940+andreyoshimura@users.noreply.github.com>
2026-04-21 12:47:19 -0300

    Improve task launcher script

--- scripts/task.sh
+++ scripts/task.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "${ROOT_DIR}"
+export PYTHONPATH="${ROOT_DIR}:${PYTHONPATH:-}"
+
 TASK_TYPE="${1:-}"
 shift || true
 
